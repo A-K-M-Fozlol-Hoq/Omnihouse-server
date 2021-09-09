c09fb176ed89169ededa75e2d5983afc88768929
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2021-09-09 11:38:08 +0600

    complete for now

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
-const { MongoClient } = require("mongodb");
+const { MongoClient,ObjectId } = require("mongodb");
 
 const port = process.env.PORT || 5000
 const app = express();
@@ -26,6 +26,133 @@ app.get('/', (req, res) => {
 
 client.connect((err) => {
     const eventsCollection = client.db("omniHouse").collection("events");
+    const draftsCollection = client.db("omniHouse").collection("drafts");
+
+    app.post("/createEvent", (req, res) => {
+      const { type, title, location, people, startTime, endTime, description, color }= req.body;
+      eventsCollection
+        .insertOne({ type, title, location, people, startTime, endTime, description, color })
+        .then((result) => {
+          console.log(result);
+          res.send(result);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    });
+
+    app.post("/createDraftEvent", (req, res) => {
+      const { type, title, location, people, startTime, endTime, description, color }= req.body;
+      draftsCollection
+        .insertOne({ type, title, location, people, startTime, endTime, description, color })
+        .then((result) => {
+          console.log(result);
+          res.send(result);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    });
+    
+    app.get("/getActiveEvents", (req, res) => {
+      let returnArray=[];
+      let index=0;
+      eventsCollection.find({ }).toArray((err, events) => {
+        if (!err) {
+          index = index + 1;
+          if (events && events.length > 0) {
+            // console.log(events)
+            for (let i = 0; i < events.length; i++) {
+              const event = events[i];
+              const eventDate= new Date(event.startTime)
+              const currentDate= new Date();
+              if(eventDate>currentDate){
+                returnArray.push(event)
+              }
+            }
+            res.send(returnArray)
+          }  else{
+            res.send(returnArray)
+          }
+        }
+      });
+    });  
+
+    app.get("/getOldEvents", (req, res) => {
+      let returnArray=[];
+      let index=0;
+      eventsCollection.find({ }).toArray((err, events) => {
+        if (!err) {
+          index = index + 1;
+          if (events && events.length > 0) {
+            // console.log(events)
+            for (let i = 0; i < events.length; i++) {
+              const event = events[i];
+              const eventDate= new Date(event.startTime)
+              const currentDate= new Date();
+              if(eventDate<currentDate){
+                returnArray.push(event)
+              }
+            }
+            res.send(returnArray)
+          }  else{
+            res.send(returnArray)
+          }
+        }
+      });
+    }); 
+    
+    app.get("/getAllEvents", (req, res) => {
+      eventsCollection.find({ }).toArray((err, events) => {
+        if (!err) {
+          if (events && events.length > 0) {
+            res.send(events)
+          }  else{
+            res.send([])
+          }
+        }
+      });
+    }); 
+
+    app.get("/getDrafts", (req, res) => {
+      draftsCollection.find({ }).toArray((err, events) => {
+        if (!err) {
+          res.send(events)
+        }else{
+          // Should handle error
+          res.send([])
+        }
+      });
+    });  
+
+    app.delete("/deleteEventByID", (req, res) => {
+      const { id }= req.body;
+      console.log(id)
+      eventsCollection
+        .deleteOne({'_id': ObjectId(`${id}`)})
+        .then((result) => {
+          console.log(result);
+          res.send(result);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    });
+
+    app.delete("/deleteDraftByID", (req, res) => {
+      const { id }= req.body;
+      console.log(id)
+      draftsCollection
+        .deleteOne({'_id': ObjectId(`${id}`)})
+        .then((result) => {
+          console.log(result);
+          res.send(result);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    });
+
 
     
     
